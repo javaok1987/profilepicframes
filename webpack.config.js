48c6d3225b80177419dba999ddabd0e48899944b
Ian <javaok1987@gmail.com>
2017-09-11 17:57:28 +0800

    Fix: 修正圖片目錄

--- webpack.config.js
+++ webpack.config.js
@@ -38,7 +38,7 @@ module.exports = {
         test: /\.(png|jpg|gif|svg)$/,
         loader: 'file-loader',
         options: {
-          name: './images/[name].[ext]?[hash]'
+          name: 'images/[name].[ext]?[hash]'
         }
       }
     ]
